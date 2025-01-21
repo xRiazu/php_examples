13040b7fc32c354f819de6e7695b3fe19e66fb36
Your Name <you@example.com>
2025-01-21 12:02:25 +0000

    2. Creating a website with multiple pages.

--- index.php
+++ index.php
@@ -1,8 +1,23 @@
 <?php
 include 'database/config.php';
-include 'partials/header.php';
+include 'components/header.php';
 ?>
-Hello World!
+
+<div class="relative font-sans before:absolute before:w-full before:h-full before:inset-0 before:bg-black before:opacity-50 before:z-10">
+      <img src="https://readymadeui.com/cardImg.webp" alt="Banner Image" class="absolute inset-0 w-full h-full object-cover" />
+
+      <div class="min-h-[350px] relative z-50 h-full max-w-6xl mx-auto flex flex-col justify-center items-center text-center text-white p-6">
+        <h2 class="sm:text-4xl text-2xl font-bold mb-6">Explore The World Of Music</h2>
+        <p class="sm:text-lg text-base text-center text-gray-200">Discover Music!</p>
+
+        <button
+          type="button"
+          class="mt-10 bg-transparent text-white text-base py-3 px-6 border border-white rounded-lg hover:bg-white hover:text-black transition duration-300">
+          Start Now!
+        </button>
+      </div>
+    </div>
+
 <?php
-include 'partials/footer.php';
+include 'components/footer.php';
 ?>
---
+++ pages/blog_list.php
@@ -0,0 +1,79 @@
+<?php
+include '../database/config.php';
+include '../components/header.php';
+?>
+
+<div class="bg-gradient-to-r from-blue-900 via-blue-800 to-blue-600 font-[sans-serif] p-4">
+      <div class="max-w-6xl mx-auto">
+        <div class="text-center max-w-xl mx-auto">
+          <h2 class="text-3xl font-extrabold text-gray-900 inline-block">LATEST BLOGS</h2>
+          <p class="text-gray-800 text-sm mt-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis accumsan, nunc et tempus blandit, metus mi consectetur felis turpis vitae ligula.</p>
+        </div>
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-12 max-lg:max-w-3xl max-md:max-w-md mx-auto">
+          <div class="bg-white cursor-pointer rounded-lg overflow-hidden group relative before:absolute before:inset-0 before:z-10 before:bg-black before:opacity-60">
+            <img src="https://readymadeui.com/Imagination.webp" alt="Blog Post 1" class="w-full h-96 object-cover group-hover:scale-110 transition-all duration-300" />
+            <div class="p-6 absolute bottom-0 left-0 right-0 z-20">
+              <span class="text-sm block mb-2 text-yellow-400 font-semibold">10 FEB 2023 | BY JOHN DOE</span>
+              <h3 class="text-xl font-bold text-white">A Guide to Igniting Your Imagination</h3>
+              <div class="mt-4">
+                <p class="text-gray-200 text-sm ">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis accumsan, nunc et tempus blandit, metus mi consectetur felis turpis vitae ligula.</p>
+              </div>
+            </div>
+          </div>
+          <div class="bg-white cursor-pointer rounded-lg overflow-hidden group relative before:absolute before:inset-0 before:z-10 before:bg-black before:opacity-60">
+            <img src="https://readymadeui.com/hacks-watch.webp" alt="Blog Post 2" class="w-full h-96 object-cover group-hover:scale-110 transition-all duration-300" />
+            <div class="p-6 absolute bottom-0 left-0 right-0 z-20">
+              <span class="text-sm block mb-2 text-yellow-400 font-semibold">7 JUN 2023 | BY MARK ADAIR</span>
+              <h3 class="text-xl font-bold text-white">Hacks to Supercharge Your Day</h3>
+              <div class="mt-4">
+                <p class="text-gray-200 text-sm ">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis accumsan, nunc et tempus blandit, metus mi consectetur felis turpis vitae ligula.</p>
+              </div>
+            </div>
+          </div>
+          <div class="bg-white cursor-pointer rounded-lg overflow-hidden group relative before:absolute before:inset-0 before:z-10 before:bg-black before:opacity-60">
+            <img src="https://readymadeui.com/prediction.webp" alt="Blog Post 3" class="w-full h-96 object-cover group-hover:scale-110 transition-all duration-300" />
+            <div class="p-6 absolute bottom-0 left-0 right-0 z-20">
+              <span class="text-sm block mb-2 text-yellow-400 font-semibold">5 OCT 2023 | BY SIMON KONECKI</span>
+              <h3 class="text-xl font-bold text-white">Trends and Predictions</h3>
+              <div class="mt-4">
+                <p class="text-gray-200 text-sm ">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis accumsan, nunc et tempus blandit, metus mi consectetur felis turpis vitae ligula.</p>
+              </div>
+            </div>
+          </div>
+          <div class="bg-white cursor-pointer rounded-lg overflow-hidden group relative before:absolute before:inset-0 before:z-10 before:bg-black before:opacity-60">
+            <img src="https://readymadeui.com/images/food.webp" alt="Blog Post 3" class="w-full h-96 object-cover group-hover:scale-110 transition-all duration-300" />
+            <div class="p-6 absolute bottom-0 left-0 right-0 z-20">
+              <span class="text-sm block mb-2 text-yellow-400 font-semibold">5 OCT 2023 | BY SIMON KONECKI</span>
+              <h3 class="text-xl font-bold text-white">Trends and Predictions</h3>
+              <div class="mt-4">
+                <p class="text-gray-200 text-sm ">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis accumsan, nunc et tempus blandit, metus mi consectetur felis turpis vitae ligula.</p>
+              </div>
+            </div>
+          </div>
+          <div class="bg-white cursor-pointer rounded-lg overflow-hidden group relative before:absolute before:inset-0 before:z-10 before:bg-black before:opacity-60">
+            <img src="https://readymadeui.com/images/food11.webp" alt="Blog Post 3" class="w-full h-96 object-cover group-hover:scale-110 transition-all duration-300" />
+            <div class="p-6 absolute bottom-0 left-0 right-0 z-20">
+              <span class="text-sm block mb-2 text-yellow-400 font-semibold">5 OCT 2023 | BY SIMON KONECKI</span>
+              <h3 class="text-xl font-bold text-white">Trends and Predictions</h3>
+              <div class="mt-4">
+                <p class="text-gray-200 text-sm ">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis accumsan, nunc et tempus blandit, metus mi consectetur felis turpis vitae ligula.</p>
+              </div>
+            </div>
+          </div>
+          <div class="bg-white cursor-pointer rounded-lg overflow-hidden group relative before:absolute before:inset-0 before:z-10 before:bg-black before:opacity-60">
+            <img src="https://readymadeui.com/images/food22.webp" alt="Blog Post 3" class="w-full h-96 object-cover group-hover:scale-110 transition-all duration-300" />
+            <div class="p-6 absolute bottom-0 left-0 right-0 z-20">
+              <span class="text-sm block mb-2 text-yellow-400 font-semibold">5 OCT 2023 | BY SIMON KONECKI</span>
+              <h3 class="text-xl font-bold text-white">Trends and Predictions</h3>
+              <div class="mt-4">
+                <p class="text-gray-200 text-sm ">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis accumsan, nunc et tempus blandit, metus mi consectetur felis turpis vitae ligula.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+<?php
+include '../components/footer.php';
+?>
