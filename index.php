5e3c19fa632db5d41ece43fd154e9d19d536eb51
Your Name <you@example.com>
2025-01-28 11:54:47 +0000

    4. changing the image to a music related image

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ include 'database/config.php';
 include 'components/header.php';
 ?>
 
-<div class="relative font-sans before:absolute before:w-full before:h-full before:inset-0 before:bg-black before:opacity-50 before:z-10">
+<div class="relative font-sans before:absolute before:w-full before:h-full before:inset-0 before:bg-black before:opacity-50 before:z-10 p-8"p-8>
       <img src="https://static.vecteezy.com/system/resources/previews/005/004/846/large_2x/abstract-sound-wave-curve-background-illustration-with-gold-and-blue-particles-creative-colorful-equalizer-and-spectrum-music-for-banner-and-poster-vector.jpg" alt="Banner Image" class="absolute inset-0 w-full h-full object-cover" />
 
       <div class="min-h-[350px] relative z-50 h-full max-w-6xl mx-auto flex flex-col justify-center items-center text-center text-white p-6">
